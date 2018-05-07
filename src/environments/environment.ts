787d100baf9f0682d9aab305912b009f957cfa8d
Rémy Halipré <rhalipre@nextoo.fr>
2018-05-07 09:46:37 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false
   api_url: 'https://ecommerce-api-dev.nextoo.fr/'
 };
