dea9e24f32eeec4a4d484754b27efe6e7befa574
Rémy Halipré <rhalipre@nextoo.fr>
2018-05-25 12:21:59 +0000

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   api_url: 'https://ecommerce-api-dev.nextoo.fr/'
+  api_rest_upload_url: 'https://ecommerce-api-dev.nextoo.fr/upload',
+  api_rest_download_url: 'https://ecommerce-api-dev.nextoo.fr/fichier/'
 };
