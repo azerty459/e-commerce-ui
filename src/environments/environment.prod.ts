70f4469f5c5c5846963586cd800c22e68e430c99
rantoine <aruffino@nextoo.fr>
2018-07-09 09:18:48 +0200

    correction back doesnt exist on produit component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api_url: 'https://ecommerce-api-prod.nextoo.fr/',
+  api_url: 'https://ecommerce-api-dev.nextoo.fr/',
   api_login_url: 'https://ecommerce-api-prod.nextoo.fr/login',
-  api_rest_upload_url: 'http://ecommerce-api-prod.nextoo.fr/upload',
-  api_rest_download_url: 'http://ecommerce-api-prod.nextoo.fr/fichier/'
+  api_rest_upload_url: 'http://java:8888/upload',
+  api_rest_download_url: 'http://java:8888/upload/fichier/'
 };
