43eee70988ec81af93fbcb8d2e6820c551ce21a3
Rémy Halipré <rhalipre@nextoo.fr>
2018-07-20 15:38:11 +0200

    param dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   api_url: 'https://ecommercedevnextoo.fr/',
   api_login_url: 'https://ecommercedev.nextoo.fr/login',
-  api_rest_upload_url: 'http://java:8888/upload',
-  api_rest_download_url: 'http://java:8888/upload/fichier/'
+  api_rest_upload_url: 'https://ecommercedev.nextoo.fr/upload',
+  api_rest_download_url: 'https://ecommercedev.nextoo.fr/upload/fichier/'
 };
