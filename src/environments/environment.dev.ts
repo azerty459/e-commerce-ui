81fc21e43e6249c2df2811d61d24ea1a520cc1f7
ADEO\20005935 <julien.agneray@ext.adeo.com>
2018-08-27 13:36:25 +0200

    Initialisation branche Julien Agneray

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: true,
-  api_url: 'https://ecommerce-api-dev.nextoo.fr/'
-  api_rest_upload_url: 'https://ecommerce-api-dev.nextoo.fr/upload',
-  api_rest_download_url: 'https://ecommerce-api-dev.nextoo.fr/fichier/'
+  production: false,
+  api_url: 'http://localhost:8888/graphql'
 };
